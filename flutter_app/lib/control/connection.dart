c4ea0d0fa023fdd82438dc0aff0b545a2f4cbb49
Allan <allanjuansil@gmail.com>
2020-03-22 19:56:28 -0300

    Criar função isConnected()

--- flutter_app/lib/control/connection.dart
+++ flutter_app/lib/control/connection.dart
@@ -1,3 +1,6 @@
+// to check internet connection
+import 'package:connectivity/connectivity.dart';
+
 class Connection {
   static String hostname() {
     var location = 'cloud'; 
@@ -14,5 +17,14 @@ class Connection {
       case 'shoppingJardins':
         return 'http://172.16.5.67:3000';
     }
+  }
+
+  static Future<bool> isConnected() async {
+    var connectivityResult = await (Connectivity().checkConnectivity()); 
+    if (connectivityResult == ConnectivityResult.mobile) 
+      return true; 
+    if (connectivityResult == ConnectivityResult.wifi)
+      return true; 
+    return false; 
   }
 }
